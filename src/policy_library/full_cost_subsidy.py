0b27cbdcbab0918c73f3abdb98e2af3ef41543ea
Ishank Juneja <ishankjuneja@gmail.com>
2024-03-25 08:05:32 -0400

    Added the ETC-CS algorithm

--- src/policy_library/full_cost_subsidy.py
+++ src/policy_library/full_cost_subsidy.py
@@ -1,15 +1,52 @@
-# Add an algorithm here that solves the full cost subsidy problem
-#  using an Improved UCB as the BAI approach in phase 1 and
-#  the pairwise elimination type scheme in the second phase to solve the problem
+import numpy as np
 
 
-def CS_ETC(p_estimates, nsamps, t, delta, mu_o, alpha):
+def CS_ETC(mu_hat, costs, nsamps, horizon, last_sampled, tau, alpha=0.0):
     """
     Implementation of the CS-ETC algorithm from the MAB-CS paper
-    :param p_estimates: array to hold mu_hat
+    https://proceedings.mlr.press/v130/sinha21a.html
+    ***********************
+    Algo explores each arm for a fixed pre-computed budget
+     and then exploits per its own exploitation rules
+    ***********************
+    :param mu_hat: Array to hold the empirical return estimates mu_hat
+    :param costs: Array to hold the costs of sampling each arm
     :param nsamps: Array to hold the number of times each arm has been sampled
-    :param t:
-    :param mu_o:
-    :param alpha:
-    :return:
+    :param horizon: Known horizon budget as input
+    :param last_sampled: Index of the last arm sampled to check if
+     already sampled sufficiently
+    :param tau: Number of times each arm is pre-explored in phase 1 of the algorithm
+    :param alpha: Subsidy factor to multiply the highest return by, lies in [0, 1]
+    :return
     """
+    # Phase 1: Pure exploration
+    # Check if the number of samples of the last sampled arm has hit tau
+    if nsamps[last_sampled] < tau:
+        # If it hasn't, then keep sampling it
+        return last_sampled
+    # Check if any arm is left to be sampled tau times
+    elif np.min(nsamps) < tau:
+        # If yes then move onto the next arm in the sequence
+        #  and sample it
+        return last_sampled + 1
+    # Move onto the UCB phase of the algorithm
+    else:
+        # Compute an array of UCB buffer terms
+        buffer = np.sqrt(2 * np.log(horizon) / nsamps)
+        # Compute the UCB values for all arms
+        ucb_values = np.max(mu_hat + buffer, np.ones_like(mu_hat))
+        # Compute the LCB values for all the arms
+        lcb_values = np.max(mu_hat - buffer, np.zeros_like(mu_hat))
+        # Compute m_t for constructing feasible set as the arm with the
+        #  highest LCB
+        m_t = np.argmax(lcb_values)
+        # Construct the feasiable set as the arm-indices of arms having
+        #  UCB above the subsidized LCB of arm m_t
+        feasible_set = np.where(ucb_values > (1 - alpha) * lcb_values[m_t])[0]
+        # Return the least cost arm in the feasible set to be sampled
+        return np.argmin(costs[feasible_set])
+
+
+# This function is going to be the phase 1 BAI step of my
+#  2 phase solution to the cost subsidy problem
+def phase1_improved_ucb()
--- src/policy_library/policy_library.py
+++ src/policy_library/policy_library.py
@@ -25,6 +25,7 @@ def ref_arm_ell_UCB(p_estimates, nsamps, t, costs, ref_arm_idx):
 
     return k
 
+
 def asymmetric_pe(ref_ell_idx, mu_hat, nsamps, horizon, delta_tilde_ell, delta_tilde, episode_num, last_sampled):
     """
     A function that implements the Asymmetric Pairwise Elimination algorithm from the notes
@@ -106,100 +107,6 @@ def asymmetric_pe(ref_ell_idx, mu_hat, nsamps, horizon, delta_tilde_ell, delta_t
                 #  need more samples from it whereas, we might not need more samples from ell immediately
                 k = episode_num
                 return k, delta_tilde_ell, delta_tilde, episode_num
-
-
-def pe_new(ref_ell_idx, mu_hat, nsamps, horizon, delta_tilde, episode_num, last_sampled):
-    """
-    A function that implements the Pairwise Elimination algorithm from the notes
-    https://www.overleaf.com/project/6502fd4306f4b073aa6bd809
-    ***********************
-    Through every execution of this function we sample a single arm
-    The round number m is not tracked explicitly, but instead it is tracked
-     implicitly using the algorithm's proxy gap tilde{Delta}
-    Case 1: All the episodes 1 through ell - 1 have been completed
-    if so, we simply sample the arm ell until the horizon budget is exhausted
-    Case 2: A candidate arm j has already been identified as the least cost acceptable arm
-    if so, we keep returning j until the horizon budget is exhausted
-    Case 3: An episode j for evaluating the candidacy of arm with index j is ongoing
-    if so, we check if the candidate arm j has been sampled n_m times already
-    if not, sample arm j until its n_m samples are accumulated
-    if yes, check if arm ell has been sampled n_m times already, and sample it if not
-    if both arms have been sampled at least n_m times, we move to the arm elimination phase
-    Comparisons in lines 14 and 18 of the algo block happen ...
-    If the current call of the function was at the cusp of a new episode (candidate arm elimination occurs),
-     then in the call we will perform the comparison and if,
-     (i) Arm j gets eliminated by arm ell, then we set next arm to be sampled to be j + 1 (j + 1 could equal ell
-     which is fine since if we have reached the end of the pack, we sample ell for the entire budget anyway)
-     (ii) Arm ell gets eliminated by arm j, then we set next arm to be sampled to be j, set the next episode
-     to be -1 to indicate that the least cost acceptable arm has been identified.
-    ***********************
-    :param ref_ell_idx: Index of the reference arm ell
-    :param mu_hat: Empirical estimates of rewards for each candidate arm and arm ell
-     Therefore the length of mu_hat is ell
-    :param nsamps: Number of times each arm has been sampled
-    :param horizon: Known horizon as input
-    :param delta_tilde: Gaps used by the elimination to set number of samples in a batch and
-     UCB buffer terms, reset for both arm ell and candidate arm j at the start of
-     every new episode
-    :param episode_num: Index of the current episode, varies between 0 and ell - 1
-    :param last_sampled: The arm that was being sampled in the previous call to the function since
-     the same arm will be sampled until we accumulate n_m samples for it
-    :return: Index of the arm to be sampled (k), updated delta_tilde, and updated episode_num
-    """
-    # Precompute horizon^e
-    horizon_e = horizon ** math.e
-    # If episode ref_ell_idx has been hit in the simulation keep returning ref_ell_idx
-    if episode_num == ref_ell_idx:
-        return ref_ell_idx, delta_tilde, episode_num
-    # If episode ell has not been hit, then check if the least cost acceptable arm has been identified
-    #  as indicated via an invalid episode number of -1
-    elif episode_num == -1:
-        # In this case the last sampled arm is the arm j that is the successful least cost candidate
-        #  and we keep returning it until the horizon budget is exhausted
-        return last_sampled, delta_tilde, episode_num
-    # Else if the episode number is a regular valid episode number then assume that the
-    #  episode in question is on going
-    else:
-        # Recompute n_m for the current episode
-        n_m = ceil(log(horizon_e * delta_tilde ** 2) / (delta_tilde ** 2))
-        # Check the number of times the episode_num indexed candidate arm has been sampled
-        if nsamps[episode_num] < n_m:
-            # If it has not been sampled n_m times, return it again with
-            #  all other parameters unchanged
-            k = episode_num
-            return k, delta_tilde, episode_num
-        # Else arm j (episode_num) must have been sampled n_m times already, so check samples of ell
-        elif nsamps[ref_ell_idx] < n_m:
-            k = ref_ell_idx
-            # If ell has not been sampled n_m times, return it with all other parameters unchanged
-            return k, delta_tilde, episode_num
-        # Else both arms have been sampled at least n_m times, so move to the arm elimination phase.
-        # conclude the episode and move to the next round within the same episode (if no elimination occurs)
-        # or move to the next episode (if an elimination occurs)
-        else:
-            # Compute the buffer terms for UCB/LCB
-            buffer = sqrt(log(horizon_e * delta_tilde ** 2) / (2 * n_m))
-            # Check if arm ell should be eliminated in favor of arm j and the episodes concluded
-            if mu_hat[ref_ell_idx] + buffer < mu_hat[episode_num] - buffer:
-                # Set episode to -1 and return arm j for sampling
-                k = episode_num
-                episode_num = -1
-                return k, delta_tilde, episode_num
-            elif mu_hat[episode_num] + buffer < mu_hat[ref_ell_idx] - buffer:
-                # Go to next episode
-                k = episode_num + 1
-                # Reset delta_tilde for the next episode
-                delta_tilde = 1.0
-                # Increment episode number
-                episode_num += 1
-                return k, delta_tilde, episode_num
-            else:
-                # Continue with the next round of the same episode by updating delta_tilde
-                delta_tilde = delta_tilde / 2
-                # Set the next arm to be sampled to be arm j since we will certainly
-                #  need more samples from it whereas, we might not need more samples from ell immediately
-                k = episode_num
-                return k, delta_tilde, episode_num
 
 
 def pairwise_elimination(ref_ell_idx, mu_hat, nsamps, horizon, delta_tilde, episode_num, last_sampled):
