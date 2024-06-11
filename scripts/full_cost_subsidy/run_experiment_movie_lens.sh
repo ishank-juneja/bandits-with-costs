5bcefeb6ce338d61021628bdfbd189927da833c0
ishank <ishankjuneja@gmail.com>
2024-06-11 16:29:44 -0400

    added script for MovieLens

---
+++ scripts/full_cost_subsidy/run_experiment_movie_lens.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Identical to the script run_experiment, but for the bandit instance inferred from MOvieLens
+# Result path variable
+OUT_FILE_PATH="results/_run_logs/full_cost_subsidy/movie_lens/"
+# Hardcoded list of files using the common path variable
+FILES=(
+    "data/ml-25m/bandit_instance.txt"
+)
+
+# Default parameters for the Python script
+STEP=500
+HORIZON=5000
+NRUNS=5
+
+# Loop through each file in the hardcoded list
+for file in "${FILES[@]}"; do
+    echo "Running simulation on $file"
+
+    # Extract the basename of the file and append "_log"
+    filename=$(basename -- "$file")
+    logname="${filename%.*}_log.csv"
+
+    python src/simulate_play/simulate_policies_fcs.py -file "$file" -STEP $STEP -horizon $HORIZON -nruns $NRUNS > "${OUT_FILE_PATH}${logname}"
+done
